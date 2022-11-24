b0d57453e2b79bdf0fbf2e1b0f87f4c5999ff126
Jonas Bäck <jonas.c.back@gmail.com>
2022-11-24 15:33:36 +0200

    Add k6 performance scripts

---
+++ 2-exercise-app/k6scripts/post.js
@@ -0,0 +1,19 @@
+import http from 'k6/http';
+
+export const options = {
+  duration: '10s',
+  vus: 10,
+  summaryTrendStats: ['avg', 'med', 'p(95)', 'p(99)'],
+};
+
+export default () => {
+  http.post(
+    'http://localhost:5000/api/submission', 
+    JSON.stringify({ exerciseId: 3, submittedCode: "abc123Code" }), 
+    {
+      headers: { 
+        'Content-Type': 'application/json',
+        Authorization: 'Bearer SomeUserToken' },
+    }
+  );
+};
