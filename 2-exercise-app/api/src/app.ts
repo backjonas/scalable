6a42b084bcc62805a8468cee39d97d04b06c35d4
Jonas Bäck <jonas.c.back@gmail.com>
2022-11-24 15:32:02 +0200

    Fix prod ui build

--- 2-exercise-app/api/src/app.ts
+++ 2-exercise-app/api/src/app.ts
@@ -1,12 +1,18 @@
 import express from 'express';
 import exerciseRouter from './routers/exercise';
 import submissionRouter from './routers/submission';
+import path from 'path';
 
 const app = express();
 app.use(express.json());
-app.use(express.static('build'));
-app.use('/api/exercise', exerciseRouter);
 app.use('/api/submission', submissionRouter);
+app.use('/api/exercise', exerciseRouter);
+app.use(express.static('build'));
+app.get('*', (req, res) => {
+  res.sendFile(path.resolve(
+    __dirname, '../build', 'index.html'
+  ));
+});
 app.use((request, response) => {
   response.sendStatus(404);
 });
